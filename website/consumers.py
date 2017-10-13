63e61b4f43b8ba064633ebab8e71483e576112cd
grez911 <grez911@gmail.com>
2017-10-13 19:51:49 +0300

    Renamed channel group to 'general' to speed up socket answers

--- website/consumers.py
+++ website/consumers.py
@@ -6,31 +6,31 @@ def ws_message(message):
     request = json.loads(message.content['text'])
     if request['operation'] == 'append_record':
         append_record(request['params']['id'])
-        Group('countbook').send({
+        Group('general').send({
             'text': get_day()
         })
     elif request['operation'] == 'get_day':
         year = request['params']['year']
         month = request['params']['month']
         day = request['params']['day']
-        Group('countbook').send({
+        Group('general').send({
             'text': get_day(year, month, day)
         })
     elif request['operation'] == 'append_operation':
         append_operation(request['params']['name'])
-        Group('countbook').send({
+        Group('general').send({
             'text': get_day()
         })
     elif request['operation'] == 'get_month':
         year = request['params']['year']
         month = request['params']['month']
-        Group('countbook').send({
+        Group('general').send({
             'text': get_month(year, month)
         })
 
 def ws_connect(message):
     message.reply_channel.send({'accept': True})
-    Group('countbook').add(message.reply_channel)
+    Group('general').add(message.reply_channel)
 
 def ws_disconnect(message):
-    Group('countbook').discard(message.reply_channel)
+    Group('general').discard(message.reply_channel)
